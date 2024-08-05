5995ef0749ec5f08a6e691282c59d825d7582488
Juan Pablo García Villa <juanpablogarciavilla2005@gmai.com>
2024-08-04 19:36:18 -0500

    Reestructuración vista de usuarios registrados

--- src/components/shared/Button/Buttons.jsx
+++ src/components/shared/Button/Buttons.jsx
@@ -34,7 +34,29 @@ export const Buttonfilter = ({Text, width, customClassName }) => {
 export const ButtomHome = ({Text, width, customClassName }) => {
   return (
     <>
-    <button className={`${customClassName} w-[${width}] mb-[2rem] mt-[1rem] text-[30px]  px-[3rem] py-[0.5rem] rounded-[10px] shadow-md `}>{Text}</button>
+   <button className={`${customClassName}   bg-white text-center w-48 rounded-2xl h-14 relative font-sans text-black text-xl font-semibold group`}
+>
+  <div
+    class="bg-[#5023A7] rounded-xl h-12 w-1/4 flex items-center justify-center absolute left-1 top-[4px] group-hover:w-[184px] z-10 duration-500"
+  >
+    <svg
+      width="20px"
+      height="20xpx"
+      viewBox="0 0 1024 1024"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill="#000000"
+        d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
+      ></path>
+      <path
+        fill="#000000"
+        d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
+      ></path>
+    </svg>
+  </div>
+  <p class="translate-x-2">Regresar</p>
+</button>
     </>
   )
 }
--- src/components/shared/InputForms/InputForms.jsx
+++ src/components/shared/InputForms/InputForms.jsx
@@ -114,4 +114,34 @@ export const
   }
   
 
+const Buscador = ({ onChange, value }) => {
+  return (
+    <div className="relative">
+      <input
+        placeholder="Buscar con documento"
+        className="input shadow-lg border-2 border-[#000000] hover:border-2 hover:border-[#000000] px-5 py-3 rounded-xl w-[20rem] transition-all outline-none"
+        name="search"
+        type="search"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      <svg
+        className="size-6 absolute top-3 right-3 text-[#000000]"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+          strokeLinejoin="round"
+          strokeLinecap="round"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
+export default Buscador;
 
